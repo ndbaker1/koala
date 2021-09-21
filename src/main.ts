0218357b661616ec15ac8283bfa1397c8cd000ef
Nick Baker <ndbaker1@outlook.com>
2021-09-21 00:41:46 -0500

    working on complex grammar in main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,96 @@ import { createParser } from "./compiler/Parser"
 import { createCPU } from "./CPU/CPU"
 import { createMemory } from "./CPU/Memory"
 
+
+const [, , ...args] = process.argv
+const input = args.find(arg => !/^--.*/.test(arg))!!
+const debug = !!args.find(arg => /--debug/.test(arg))
+
+// const parser = createParser(Grammar)
+
+// const instructions = parser.parse(input)
+// const memory = createMemory()
+// const cpu = createCPU({ memory, debug })
+// memory.load(instructions)
+// cpu.run()
+
+
+
+
+const complexGrammar = `
+
+PROGRAM
+  = FUNCDEF PROGRAM
+  / stmts:STMTS prog:PROGRAM        { return stmts.concat(prog) }
+  / func_def:FUNCDEF prog:PROGRAM   { return [func_def].concat(prog) }
+  / FUNCDEF
+  / STMTS
+
+FUNCDEF
+  = "fun" WHITESPACE func_def:IDENTIFIER "(" ")" WHITESPACE "{" LINEEND stmts:STMTS "}"     { return { func_def, stmts } }
+STMTS
+  = stmt:STMT stmts:STMTS   { return [stmt].concat(stmts) }
+  / stmt:STMT               { return [stmt] }
+STMT
+  = WHITESPACE "if" "(" cond:EXPR ")" true_branch:STMT "else" false_branch:STMT         { return { action: 'if', true_branch, false_branch } }
+  / WHITESPACE "if" "(" EXPR ")" STMT
+  / WHITESPACE "while" "(" EXPR ")" STMT
+  / WHITESPACE "print" "(" expr:EXPR ")" LINEEND                                        { return { action: 'print', expr } }
+  / WHITESPACE id:IDENTIFIER WHITESPACE action:ASSIGN WHITESPACE expr:EXPR LINEEND      { return { id, action, expr } }
+  / WHITESPACE "return" LINEEND
+  / WHITESPACE "return" WHITESPACE EXPR LINEEND
+  / WHITESPACE IDENTIFIER "++" LINEEND
+  / WHITESPACE IDENTIFIER "--" LINEEND
+  / WHITESPACE "{" STMTS "}"
+EXPRLIST
+  = EXPR EXPRLIST
+  / EXPR 
+EXPR
+  = id:IDENTIFIER WHITESPACE binop:BINARYOP WHITESPACE expr:EXPR                { return { id, binop, expr } }
+  / id:IDENTIFIER WHITESPACE "(" ARGS ")" binop:BINARYOP WHITESPACE expr:EXPR   { return { id, binop, expr } }
+  / str:STRINGLIT WHITESPACE binop:BINARYOP WHITESPACE expr:EXPR                { return { str, binop, expr } }
+  / int:INTLIT WHITESPACE binop:BINARYOP WHITESPACE expr:EXPR                   { return { int, binop, expr } }
+  / IDENTIFIER
+  / IDENTIFIER "(" ARGS ")"
+  / STRINGLIT                                                                   { return text() }
+  / INTLIT                                                                      { return parseInt(text()) }
+
+ARGS
+  = EXPR "," ARGS
+  / EXPR
+
+BINARYOP
+  = ("*"/"/"/"+"/"-"/"<"/">"/"<="/">="/"=="/"<>"/"||"/"&&") { return text() }
+
+ASSIGN = "="                        { return text() }
+IDENTIFIER = [a-zA-Z][a-zA-Z0-9]*   { return text() }
+
+INTLIT = [0-9]+ 
+STRINGLIT = "\'" [a-zA-Z0-9 ]* "\'" 
+
+LINEEND = WHITESPACE NEWLINE
+NEWLINE = [\\r\\n]+
+WHITESPACE = [ ]*
+`
+
+const code = `
+
+c = 'string'
+print(c)
+
+fun main() {   
+  a = 5
+  a = a + 5
+  print(a)
+}
+
+x = 55
+
+`
+
+const complexParser = createParser(complexGrammar, { debug })
+console.log(JSON.stringify(complexParser.parse(code), null, 2))
+
 /*
 
 fun main() {
@@ -17,7 +107,7 @@ fun main() {
   print(number)
 
   print(text)
-  
+
 }
 
 
@@ -28,15 +118,6 @@ fun recursion(val: int) {
       else -> { return recursion(val--) }
     }
   }
-}
-
-*/
-
-/*
-
-fun main() {
-  a = 4
-  print(a)
 }
 
 */
@@ -77,29 +158,3 @@ ARGS:
 
 */
 
-const [, , ...args] = process.argv
-const input = args.find(arg => !/^--.*/.test(arg))!!
-const debug = !!args.find(arg => /--debug/.test(arg))
-
-const parser = createParser(
-  `
-  {{${Object.values(Grammar).reduce((acc, cur) => acc + cur.toString(), '')}}}
-  
-  Program
-    = expr:Expression { return ${Grammar.Program}(expr) }
-  Expression
-    = head:Term "+" tail:Term { return ${Grammar.Expression}('+', head, tail) }
-    / head:Term "-" tail:Term { return ${Grammar.Expression}('-', head, tail) }
-  Term
-    = "(" expr:Expression ")" { return expr; }
-    / Integer
-  Integer
-    = [0-9]+ { return ${Grammar.Integer}(parseInt(text(), 10)) }
-  `
-)
-
-const instructions = parser.parse(input)
-const memory = createMemory()
-const cpu = createCPU({ memory, debug })
-memory.load(instructions)
-cpu.run()
