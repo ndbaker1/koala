0a465279aaa1a8f9d1ffedfdeda7edc44e5891eb
Nick Baker <ndbaker1@outlook.com>
2021-09-28 01:24:31 -0500

    updates ASTTypes to have name and function

--- site/examples.ts
+++ site/examples.ts
@@ -59,31 +59,29 @@ fun recursion(val) {
   }
 }`
 
-export const ASTTypes = {
-  test: function (node: any) { },
-  functionDefinition: 'function_def',
-  functionCall: 'function_call',
-  ifControl: 'if',
-  ifElseControl: 'if_else',
-  functionReturn: 'return',
-  functionReturnExpr: 'return_expr',
-  whileControl: 'while',
-  expr: 'expr',
-  binaryExpr: 'binary_expr',
-  variable: 'variable',
-  bool: 'bool',
-  string: 'str',
-  int: 'int',
-  print: 'print',
-  assignment: 'assignment',
-  increment: 'increment',
-  decrement: 'decrement',
-
-  when: 'when',
-  whenCaseStmt: 'when_case_stmt',
-  whenCaseExpr: 'when_case_expr',
-  whenElseStmt: 'when_else_stmt',
-  whenElseExpr: 'when_else_expr',
+export const ASTTypes: Record<string, { name: string, function: (node: any) => void }> = {
+  functionDefinition: { name: 'function_def', function: function (node) { } },
+  functionCall: { name: 'function_call', function: function (node) { } },
+  if: { name: 'if', function: function (node) { } },
+  ifElse: { name: 'if_else', function: function (node) { } },
+  return: { name: 'return', function: function (node) { } },
+  returnExpr: { name: 'return_expr', function: function (node) { } },
+  while: { name: 'while', function: function (node) { } },
+  expr: { name: 'expr', function: function (node) { } },
+  binaryExpr: { name: 'binary_expr', function: function (node) { } },
+  variable: { name: 'variable', function: function (node) { } },
+  bool: { name: 'bool', function: function (node) { } },
+  string: { name: 'str', function: function (node) { } },
+  int: { name: 'int', function: function (node) { } },
+  print: { name: 'print', function: function (node) { } },
+  assignment: { name: 'assignment', function: function (node) { } },
+  increment: { name: 'increment', function: function (node) { } },
+  decrement: { name: 'decrement', function: function (node) { } },
+  when: { name: 'when', function: function (node) { } },
+  whenCaseStmt: { name: 'when_case_stmt', function: function (node) { } },
+  whenCaseExpr: { name: 'when_case_expr', function: function (node) { } },
+  whenElseStmt: { name: 'when_else_stmt', function: function (node) { } },
+  whenElseExpr: { name: 'when_else_expr', function: function (node) { } },
 }
 
 export const GrammarExample = `PROGRAM
@@ -96,9 +94,9 @@ export const GrammarExample = `PROGRAM
 
 FUNC_DEF
   = WS "fun" WS id:IDENTIFIER "(" WS params:PARAMETERS WS ")" WS stmts:BLOCK
-    { return { type: '${ASTTypes.functionDefinition}', id: id.id, params, stmts } }
+    { return { type: '${ASTTypes.functionDefinition.name}', id: id.id, params, stmts } }
   / WS "fun" WS id:IDENTIFIER "(" ")" WS stmts:BLOCK
-    { return { type: '${ASTTypes.functionDefinition}', id: id.id, params: [], stmts } }
+    { return { type: '${ASTTypes.functionDefinition.name}', id: id.id, params: [], stmts } }
 STMTS
   = stmt:STMT LINEEND stmts:STMTS
     { return [stmt].concat(stmts) }
@@ -106,25 +104,25 @@ STMTS
     { return [stmt] }
 STMT
   = WS "if" WS "(" cond:EXPR ")" WS true_branch:BLOCK WS "else" WS false_branch:BLOCK
-    { return { type: '${ASTTypes.ifElseControl}', cond, true_branch, false_branch } }
+    { return { type: '${ASTTypes.ifElseControl.name}', cond, true_branch, false_branch } }
   / WS "if" "(" cond:EXPR ")" WS stmts:BLOCK
-    { return { type: '${ASTTypes.ifControl}', cond, stmts } }
+    { return { type: '${ASTTypes.ifControl.name}', cond, stmts } }
   / WS when:WHEN
     { return when }
   / WS "while" "(" cond:EXPR ")" WS stmts:BLOCK
-    { return { type: '${ASTTypes.whileControl}', cond, stmts } }
+    { return { type: '${ASTTypes.whileControl.name}', cond, stmts } }
   / WS "print" "(" expr:EXPR ")"
-    { return { type: '${ASTTypes.print}', expr } }
+    { return { type: '${ASTTypes.print.name}', expr } }
   / WS id:IDENTIFIER WS type:ASSIGN WS expr:EXPR
-    { return { id, type: '${ASTTypes.assignment}', expr } }
+    { return { id, type: '${ASTTypes.assignment.name}', expr } }
   / WS "return" WS expr:EXPR
-    { return { type: '${ASTTypes.functionReturnExpr}', expr } }
+    { return { type: '${ASTTypes.functionReturnExpr.name}', expr } }
   / WS "return"
-    { return { type: '${ASTTypes.functionReturn}' } }
+    { return { type: '${ASTTypes.functionReturn.name}' } }
   / WS id:IDENTIFIER "++"
-    { return { type: '${ASTTypes.increment}', id } }
+    { return { type: '${ASTTypes.increment.name}', id } }
   / WS id:IDENTIFIER "--"
-    { return { type: '${ASTTypes.decrement}', id } }
+    { return { type: '${ASTTypes.decrement.name}', id } }
   / WS block:BLOCK
     { return block }
 BLOCK
@@ -132,7 +130,7 @@ BLOCK
     { return stmts }
 WHEN
   = "when" WS "(" expr:EXPR ")" WS "{" LINEEND when_cases:WHENCASES LINEEND WS "}"
-    { return { type: '${ASTTypes.when}', expr, when_cases } }
+    { return { type: '${ASTTypes.when.name}', expr, when_cases } }
 WHENCASES
   = when_case:WHENCASE LINEEND when_cases:WHENCASES
     { return [when_case].concat(when_cases) }
@@ -140,13 +138,13 @@ WHENCASES
     { return [when_case] }
 WHENCASE
   = WS case_expr:EXPR WS "->" WS case_value:STMT
-    { return { type: '${ASTTypes.whenCaseStmt}', case_expr, case_value } }
+    { return { type: '${ASTTypes.whenCaseStmt.name}', case_expr, case_value } }
   / WS case_expr:EXPR WS "->" WS case_value:EXPR
-    { return { type: '${ASTTypes.whenCaseExpr}', case_expr, case_value } }
+    { return { type: '${ASTTypes.whenCaseExpr.name}', case_expr, case_value } }
   / WS "else" WS "->" WS case_value:STMT
-    { return { type: '${ASTTypes.whenElseStmt}', case_value } }
+    { return { type: '${ASTTypes.whenElseStmt.name}', case_value } }
   / WS "else" WS "->" WS case_value:EXPR
-    { return { type: '${ASTTypes.whenElseExpr}', case_value } }
+    { return { type: '${ASTTypes.whenElseExpr.name}', case_value } }
 EXPR
   = "(" WS expr:EXPR WS ")"
     { return expr }
@@ -159,13 +157,13 @@ EXPR
 
 BINARY_EXPR
   = fun_call:FUNC_CALL WS binop:BINARYOP WS expr:EXPR
-    { return { type: '${ASTTypes.binaryExpr}', binop, args: [fun_call, expr] } }
+    { return { type: '${ASTTypes.binaryExpr.name}', binop, args: [fun_call, expr] } }
   / id:IDENTIFIER WS binop:BINARYOP WS expr:EXPR
-    { return { type: '${ASTTypes.binaryExpr}', binop, args: [id, expr] } }
+    { return { type: '${ASTTypes.binaryExpr.name}', binop, args: [id, expr] } }
   / str:STRINGLIT WS binop:BINARYOP WS expr:EXPR
-    { return { type: '${ASTTypes.binaryExpr}', binop, args: [str, expr] } }
+    { return { type: '${ASTTypes.binaryExpr.name}', binop, args: [str, expr] } }
   / int:INTLIT WS binop:BINARYOP WS expr:EXPR
-    { return { type: '${ASTTypes.binaryExpr}', binop, args: [int, expr] } }
+    { return { type: '${ASTTypes.binaryExpr.name}', binop, args: [int, expr] } }
 
 PARAMETERS
   = id:IDENTIFIER WS "," WS params:PARAMETERS
@@ -185,18 +183,18 @@ ASSIGN = "="
 
 FUNC_CALL
   = id:IDENTIFIER "(" WS args:ARGS WS ")"
-    { return { type: '${ASTTypes.functionCall}', id: id.id, args } }
+    { return { type: '${ASTTypes.functionCall.name}', id: id.id, args } }
   / id:IDENTIFIER "(" ")"
-    { return { type: '${ASTTypes.functionCall}', id: id.id, args: [] } }
+    { return { type: '${ASTTypes.functionCall.name}', id: id.id, args: [] } }
 
 IDENTIFIER = [a-zA-Z][a-zA-Z0-9]*
-  { return { type: '${ASTTypes.variable}', id: text() } }
+  { return { type: '${ASTTypes.variable.name}', id: text() } }
 INTLIT = "-"? [0-9]+
-  { return { type: '${ASTTypes.int}', value: parseInt(text()) } }
+  { return { type: '${ASTTypes.int.name}', value: parseInt(text()) } }
 STRINGLIT = "'" [a-zA-Z0-9 \\n\\r\\t]* "'"
-  { return { type: '${ASTTypes.string}', value: text() } }
+  { return { type: '${ASTTypes.string.name}', value: text() } }
 BOOLLIT = ("true"/"false")
-  { return { type: '${ASTTypes.bool}', value: Boolean(text()) } }
+  { return { type: '${ASTTypes.bool.name}', value: Boolean(text()) } }
 
 LINEEND = WS NL
 NL = [\\r\\n]+
@@ -218,7 +216,7 @@ let stackOffset = 0
 // the example grammar handles types like this
 ${objectToVarDecls(ASTTypes)} 
 // real work
-for (const node of ASTRoot) codeGen[node.type](node)
+for (const node of ASTRoot) codeGens[node.type](node)
 return code + 50
 
 `
